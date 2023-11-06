0205ee7417b1b3d5aaed06ef7e67df2a7ce24bad
Andrea Castro <andreagcastroe72@gmail.com>
2023-11-05 18:18:22 -0600

    Avance 1.2

--- Grupo4-Proyecto-CGV/Assets/Scripts/PlayerController.cs
+++ Grupo4-Proyecto-CGV/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,17 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalMove;
     public float verticalMove;
+    private Vector3 playerInput;
+
     public CharacterController player;
 
     public float playerSpeed;
-    
+    private Vector3 movePlayer;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +29,30 @@ public class PlayerController : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3 (horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        player.Move(movePlayer * playerSpeed * Time.deltaTime);
+
+        Debug.Log(player.velocity.magnitude);
     }
 
-    private void FixedUpdate()
+    void camDirection()
     {
-        player.Move(new Vector3(horizontalMove, 0, verticalMove) * playerSpeed * Time.deltaTime);
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 }
