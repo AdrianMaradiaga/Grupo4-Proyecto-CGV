69e45ef4ad99d48cfd4b25dea98ab9b5a253ca41
FabsValladares <99628986+FabsValladares@users.noreply.github.com>
2023-11-08 07:23:14 -0600

    Nuevas rampas
    Nuevas rampas

--- Grupo4-Proyecto-CGV/Assets/Scripts/PlayerController.cs
+++ Grupo4-Proyecto-CGV/Assets/Scripts/PlayerController.cs
@@ -4,94 +4,102 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    // Variables públicas para ajustar parámetros
     public float horizontalMove;
     public float verticalMove;
     private Vector3 playerInput;
-    public float gravity = 22f;
-    public float fallVelocity;
-    public float jumpForce = 8;
+    public float gravity = 22f;        // Gravedad aplicada al personaje
+    public float fallVelocity;         // Velocidad de caída
+    public float jumpForce = 8;        // Fuerza de salto
 
-    public CharacterController player;
+    public CharacterController player; // Componente CharacterController que controla al personaje
 
-    public float playerSpeed;
-    private Vector3 movePlayer;
+    public float playerSpeed;          // Velocidad de movimiento del jugador
+    private Vector3 movePlayer;       // Vector de movimiento
 
-    public Camera mainCamera;
-    private Vector3 camForward;
-    private Vector3 camRight;
+    public Camera mainCamera;          // Cámara principal
+    private Vector3 camForward;       // Dirección hacia adelante de la cámara
+    private Vector3 camRight;         // Dirección hacia la derecha de la cámara
 
-    // Start is called before the first frame update
+    // Método llamado al inicio del juego
     void Start()
     {
+        // Inicializa el componente CharacterController
         player = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+    // Método llamado en cada fotograma del juego
     void Update()
     {
+        // Obtener las entradas del jugador para el movimiento
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
 
-        playerInput = new Vector3 (horizontalMove, 0, verticalMove);
+        // Crear un vector de entrada del jugador y limitar su magnitud a 1
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
         playerInput = Vector3.ClampMagnitude(playerInput, 1);
 
+        // Calcular la dirección de la cámara en relación al personaje
         camDirection();
 
+        // Calcular el vector de movimiento del jugador
         movePlayer = playerInput.x * camRight + playerInput.z * camForward;
-
         movePlayer = movePlayer * playerSpeed;
 
+        // Hacer que el personaje mire en la dirección del movimiento
         player.transform.LookAt(player.transform.position + movePlayer);
 
+        // Aplicar la gravedad
         SetGravity();
 
+        // Gestionar las habilidades del jugador, como el salto
         PlayerSkills();
 
+        // Mover al jugador en base al vector de movimiento
         player.Move(movePlayer * Time.deltaTime);
 
+        // Imprimir la magnitud de la velocidad del jugador en la consola
         Debug.Log(player.velocity.magnitude);
     }
 
+    // Calcular la dirección de la cámara
     void camDirection()
     {
         camForward = mainCamera.transform.forward;
         camRight = mainCamera.transform.right;
 
+        // Anular los componentes de altura (y) y normalizar los vectores
         camForward.y = 0;
         camRight.y = 0;
-
         camForward = camForward.normalized;
         camRight = camRight.normalized;
     }
 
-    //Funcion para las habilidades de nuestro jugador.
+    // Función para las habilidades del jugador
     public void PlayerSkills()
     {
-        if (player.isGrounded && Input.GetButtonDown("Jump")) 
-        { 
+        // Verificar si el jugador está en el suelo y presiona el botón de salto
+        if (player.isGrounded && Input.GetButtonDown("Jump"))
+        {
             fallVelocity = jumpForce;
-            movePlayer.y = fallVelocity; 
+            movePlayer.y = fallVelocity;
         }
     }
 
-    //Funcion para la gravedad
-    void SetGravity(){
-
-        
-
+    // Función para gestionar la gravedad
+    void SetGravity()
+    {
         if (player.isGrounded)
         {
+            // El jugador está en el suelo, restablecer la velocidad de caída
             fallVelocity = -gravity * Time.deltaTime;
             movePlayer.y = fallVelocity;
         }
         else
         {
+            // El jugador no está en el suelo, aplicar la gravedad
             fallVelocity -= gravity * Time.deltaTime;
-            movePlayer.y = fallVelocity; 
-
-
+            movePlayer.y = fallVelocity;
         }
-
     }
 }
