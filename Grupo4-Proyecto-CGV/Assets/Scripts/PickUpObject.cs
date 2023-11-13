68a3f8b294e490b4c5b9b5304f904dbb25cc26c3
AshlyAlvarado <Ashlyalva@gmail.com>
2023-11-13 16:36:08 -0600

    Agarrar Objetos

---
+++ Grupo4-Proyecto-CGV/Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+
+    public GameObject ObjectToPickUp;
+    public GameObject PickedObject;
+    public Transform interationZone;
+
+    void Update()
+    {
+        if (ObjectToPickUp != null && ObjectToPickUp.GetComponent<PickableObject>().isPickable == true && PickedObject == null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickedObject = ObjectToPickUp;
+                PickedObject.GetComponent<PickableObject>().isPickable = false;
+                PickedObject.transform.SetParent(interationZone);
+                PickedObject.transform.position = interationZone.position;
+                PickedObject.GetComponent<Rigidbody>().useGravity = false;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+        else if (PickedObject != null)
+        { 
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickedObject.GetComponent<PickableObject>().isPickable = true;
+                PickedObject.transform.SetParent(null);
+                PickedObject.GetComponent<Rigidbody>().useGravity = true;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                PickedObject = null;
+            }
+        }
+    }
+}
